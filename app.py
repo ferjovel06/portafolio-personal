8193ebdf3da967863be335795ba14e6db36559c3
ferjovel06 <fermajobe@gmail.com>
2023-08-31 10:37:54 -0600

    Anhadir tsparticles

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
 # Create an instance of Flask
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
 
 # Create a route / that renders home.html template
 @app.route('/')
